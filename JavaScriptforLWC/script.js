5527683e102330629e58fba6b9ef47917a868f40
SwatiMachhewar <07swati98@gmail.com>
2025-01-25 14:54:15 +0000

    learn

---
+++ JavaScriptforLWC/script.js
@@ -0,0 +1,6 @@
+var course = "Zero to Hero in LWC...."
+console.log(course)
+course = "Zero to hero in Salesforce LWC!!!"
+console.log(course)
+var course = "Zero to Hero in LWC to learn Salesforce Development..."
+console.log(course)
